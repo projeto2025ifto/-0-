5e7a346bca6c11bc487466f028a8f4c08cf56fa0
projetoifto <projeto2025ifto@gmail.com>
2025-02-14 10:51:02 -0300

    001-2

--- setup/settings.py
+++ setup/settings.py
@@ -110,3 +110,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Certifique-se de que a pasta 'static' existe
+]
