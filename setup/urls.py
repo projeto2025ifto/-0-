86c99be304ccdcbfe104fedd8289ad3b239eba7e
projetoifto <projeto2025ifto@gmail.com>
2025-02-14 11:09:03 -0300

    001-4

--- setup/urls.py
+++ setup/urls.py
@@ -11,5 +11,10 @@ urlpatterns = [
     path('', include('painel.urls')),
     path('', include('informe.urls')),
 ]
+# Adicione isso para servir arquivos estáticos em ambiente de desenvolvimento
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Isso já está incluindo as mídias, mas faltava estáticos
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
